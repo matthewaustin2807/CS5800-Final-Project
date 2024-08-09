40d2606fd855e292f3699b94dde8bd9a2dc539e9
Matthew Chandra <matthewaustin2807@gmail.com>
2024-08-08 18:47:08 -0700

    Added pause functionality and step by step functionality to GUI

--- gui.py
+++ gui.py
@@ -9,7 +9,7 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 squareSize = 50
-# example_input = [[0, 0], [1, 2], [0, 4], [2, 3], [4, 4], [3, 2], [4, 0], [2, 1], [0, 2], [1, 0], [3, 1], [4, 3], [2, 4], [0, 3], [1, 1], [3, 0], [4, 2], [3, 4], [1, 3], [0, 1], [2, 0], [4, 1], [2, 2], [1, 4], [3, 3]]
+example_input = [[0, 0], [1, 2], [0, 4], [2, 3], [4, 4], [3, 2], [4, 0], [2, 1], [0, 2], [1, 0], [3, 1], [4, 3], [2, 4], [0, 3], [1, 1], [3, 0], [4, 2], [3, 4], [1, 3], [0, 1], [2, 0], [4, 1], [2, 2], [1, 4], [3, 3]]
 
 class ChessBoard():
     """ChessBoard class which will generate a chess board of a given size and start the Knight's Tour visualization
@@ -34,6 +34,17 @@ class ChessBoard():
         self.buttonPress = 0
         self.buttons = {}
         self.all_knight_moves = all_knight_moves
+        self.prevGrid = None
+        self.icon = self.loadImage('knight.png')
+        self.image_label = None
+        self.paused = False
+    
+    def pause(self):
+        """Callback function to pause visualization
+        """
+        self.paused = True
+        self.buttons['play'].config(state=ACTIVE)
+        self.buttons['pause'].config(state=DISABLED)
         
     def populateButtons(self):
         """Populate an area on the Tkinter Window for Clickable Buttons to Start, Restart or Quit the Visualization
@@ -44,19 +55,29 @@ class ChessBoard():
         buttonsArea['bg'] = buttonsArea.master['bg']
         buttonsArea.pack(side=BOTTOM)
         
+        # Start Button
+        startButton = Button(master=buttonsArea, text="Play", command=self.play,borderwidth=3, relief=RAISED, width=10, height=2, bg='seashell3')
+        startButton.grid(row=0, column=0, sticky='nsew')
+        self.buttons['play'] = startButton
+        
         # Next Button
-        nextButton = Button(master=buttonsArea, text="Start", command=self.fillGrids,borderwidth=3, relief=RAISED, width=10, height=2, bg='SeaGreen2')
-        nextButton.grid(row=0, column=0, sticky='nsew')
-        self.buttons['start'] = nextButton
+        nextButton = Button(master=buttonsArea, text="Next", command=self.nextMove, borderwidth=3, relief=RAISED, width=10, height=2, bg='seashell3')
+        nextButton.grid(row=0, column=1, sticky='nsew')
+        self.buttons['next'] = nextButton
+        
+        # Pause Button
+        pauseButton = Button(master=buttonsArea, text="Pause", command=self.pause, borderwidth=3, relief=RAISED, width=10, height=2, bg='seashell3', state=DISABLED)
+        pauseButton.grid(row=0, column=2, sticky='nsew')
+        self.buttons['pause'] = pauseButton
         
         # Restart Button
-        restartButton = Button(master=buttonsArea, text="Restart", command=self.restartProgram, borderwidth=3, relief=RAISED, width=10, height=2, state=DISABLED, bg='OliveDrab1')
-        restartButton.grid(row=0, column=1, sticky='nsew')
+        restartButton = Button(master=buttonsArea, text="Restart", command=self.restartProgram, borderwidth=3, relief=RAISED, width=10, height=2, state=DISABLED, bg='seashell3')
+        restartButton.grid(row=0, column=3, sticky='nsew')
         self.buttons['restart'] = restartButton
         
         # Quit Button
-        quitButton = Button(master=buttonsArea, text="Quit", command=self.quitProgram, borderwidth=3, relief=RAISED, width=10, height=2, bg='coral1')
-        quitButton.grid(row=0, column=2, sticky='nsew')
+        quitButton = Button(master=buttonsArea, text="Quit", command=self.quitProgram, borderwidth=3, relief=RAISED, width=10, height=2, bg='seashell3')
+        quitButton.grid(row=0, column=4, sticky='nsew')
         self.buttons['quit'] = quitButton
             
     def populateGrids(self):
@@ -116,50 +137,68 @@ class ChessBoard():
         image = image.resize((25, 25), Image.NEAREST)
         return ImageTk.PhotoImage(image)
 
-    def fillGrids(self):  
-        """Fills in the grids one at a time based on the passed in knight's move
+    def nextMove(self):        
+        """Move the Knight on the ChessBoard to the next Position
         """
-        icon = self.loadImage('knight.png')
-        
-        # Disable Start Button
-        self.buttons['start'].config(state=DISABLED)
-        prevGrid = None
-        
-        # Mark Knight's Move Frame by Frame
-        while self.buttonPress < self.rows*self.columns:
-            # Label previous move as an X on the board
-            if prevGrid is not None:
-                image_label.destroy()
-                x_label = Label(master=self.board_status[entry[0]][entry[1]], text="X", anchor=CENTER, font=('bold'))
+        self.buttons['restart'].config(state=ACTIVE)
+        # Mark the previously visited square with the move number
+        if self.prevGrid is not None:
+                self.image_label.destroy()
+                x_label = Label(master=self.board_status[self.prevGrid[0]][self.prevGrid[1]], text=f'{self.buttonPress - 1}', anchor=CENTER, font=('bold'))
                 x_label['bg'] = x_label.master['bg']
                 x_label.pack(fill="both", expand=1)
-            
-            # Get the Knight's move based on the move number
-            entry = self.all_knight_moves[self.buttonPress]
-            image_label = Label(master=self.board_status[entry[0]][entry[1]], image=icon, anchor=CENTER)
-            image_label['bg'] = image_label.master['bg']
-            image_label.pack(fill="both", expand=1)
-            prevGrid = [entry[0], entry[1]]
-            self.buttonPress += 1
-            
-            # Enables Frame by Frame Visualization
-            time.sleep(0.5)
-            self.root.update()
-    
-        # Last Move to be Marked by an X before Completion
-        image_label.destroy()
-        x_label = Label(master=self.board_status[entry[0]][entry[1]], text="X", anchor=CENTER, font=('bold'))
-        x_label['bg'] = x_label.master['bg']
-        x_label.pack(fill="both", expand=1)
-       
-        time.sleep(0.5)
-        self.root.update()
+         
+        # Get the move from the move list
+        move = self.all_knight_moves[self.buttonPress]
         
-        # Enable Restart Button and Display Complete Message
-        self.buttons['restart'].config(state=ACTIVE)
+        # Set the Knight on the correct square on the board
+        self.image_label = Label(master=self.board_status[move[0]][move[1]], image=self.icon, anchor=CENTER)
+        self.image_label['bg'] = self.image_label.master['bg']
+        self.image_label.image = self.icon
+        self.image_label.pack(fill="both", expand=1)
+        
+        # Set the current visited grid as the previous grid
+        self.prevGrid = move
+        
+        # Increment button press
+        self.buttonPress += 1
+        
+        # If the number of button press equals the total moves, finish algorithm.
+        if self.buttonPress == len(self.all_knight_moves):
+            self.image_label.destroy()
+            x_label = Label(master=self.board_status[self.prevGrid[0]][self.prevGrid[1]], text=f'{self.buttonPress - 1}', anchor=CENTER, font=('bold'))
+            x_label['bg'] = x_label.master['bg']
+            x_label.pack(fill="both", expand=1)
+            
+            self.displayEndMessage()
+        
+    def displayEndMessage(self):
+        """State that the algorithm finished running and disable pause and next buttons
+        """
+        self.buttons['next'].config(state=DISABLED)
+        self.buttons['pause'].config(state=DISABLED)
         done_label = Label(master=self.root, text="Knight's Tour Complete", pady=15)
         done_label['bg'] = done_label.master['bg']
         done_label.pack()
+        
+        
+    def play(self):  
+        """Automatically moves the knights across the chessboard according to the moves from the algorithm.
+        """
+        # Disable Start Button, Enable Pause Button and set Paused to False
+        self.buttons['play'].config(state=DISABLED)
+        self.buttons['pause'].config(state=ACTIVE)
+        self.paused = False
+        
+        # Mark Knight's Move Frame by Frame
+        while self.buttonPress < self.rows*self.columns:
+            # If paused button is pressed, stop
+            if self.paused:
+                return
+            self.nextMove()        
+            # Enables Frame by Frame Visualization
+            time.sleep(0.5)
+            self.root.update()
     
     def quitProgram(self):
         """Quits the Visualization Program
@@ -173,6 +212,7 @@ class ChessBoard():
             widget.destroy()
         self.buttons.clear()
         self.buttonPress = 0
+        self.prevGrid = None
         self.runVisualization()
         
     def runVisualization(self):
@@ -182,17 +222,17 @@ class ChessBoard():
         self.populateButtons()
 
 # Test Method
-# def main():
-#     root = tk.Tk()
-#     root.title("Knight's Tour Visualization")
-#     root.minsize(width=400, height=400)
-#     root.configure(background="papaya whip")
+def main():
+    root = tk.Tk()
+    root.title("Knight's Tour Visualization")
+    root.minsize(width=400, height=400)
+    root.configure(background="papaya whip")
     
-#     chessBoard = ChessBoard(5, 5, root, example_input)
-#     chessBoard.runVisualization()
+    chessBoard = ChessBoard(5, 5, root, example_input)
+    chessBoard.runVisualization()
     
-#     root.mainloop()
+    root.mainloop()
 
     
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
