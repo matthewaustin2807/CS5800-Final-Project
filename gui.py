445c2027ecbf38dc65d24cee1acb7a409e517b69
Matthew Chandra <matthewaustin2807@gmail.com>
2024-08-12 17:06:33 -0700

    fixed bug in gui

--- gui.py
+++ gui.py
@@ -14,6 +14,9 @@ squareSize = 50  # Size of each square on the chessboard
 # Example inputs for testing the GUI (these would typically come from the main algorithm)
 example_input = [[0, 0], [1, 2], [0, 4], [2, 3], [4, 4], [3, 2], [4, 0], [2, 1], [0, 2], [1, 0], [3, 1], [4, 3], [2, 4], [0, 3], [1, 1], [3, 0], [4, 2], [3, 4], [1, 3], [0, 1], [2, 0], [4, 1], [2, 2], [1, 4], [3, 3]]
 example_no_sol_input = [[0, 0], [1, 2], [2, 0], [0, 1], [2, 2], [1, 0], [0, 2], [2, 1], 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', [2, 1], [0, 2], [1, 0], [2, 2], [0, 1], [2, 0], [1, 2], 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack']
+example_input_2 = [[2, 2], [0, 1], [1, 3], [3, 4], [5, 3], [7, 4], [6, 2], [7, 0], [5, 1], [3, 0], [1, 1], [0, 3], [2, 4], [4, 3], [6, 4], [7, 2], [6, 0], [4, 1], [2, 0], [3, 2], [4, 0], [6, 1], [7, 3], [5, 4], [3, 3], [1, 4], [0, 2], [1, 0], [3, 1], [1, 2], [0, 0], [2, 1], [4, 2], [5, 0], [7, 1], [5, 2], [4, 4], [6, 3], 'backtrack', [2, 3], [0, 4], 'backtrack', 'backtrack', 'backtrack', 'backtrack', [6, 3], [4, 4], [5, 2], 'backtrack', [2, 3], [0, 4], 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', [2, 3], [0, 4], 'backtrack', [4, 4], [5, 2], [7, 1], [5, 0], 'backtrack', [6, 3], 'backtrack', 'backtrack', 'backtrack', [6, 3], [7, 1], [5, 0], 'backtrack', [5, 2], 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', [6, 3], [4, 4], [2, 3], [0, 4], 'backtrack', 'backtrack', [5, 2], [7, 1], [5, 0], 'backtrack', 'backtrack', 'backtrack', 'backtrack', [7, 1], [5, 0], 'backtrack', [5, 2], [4, 4], [2, 3], [0, 4], 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', [0, 4], [2, 3], [4, 4], [5, 2], [7, 1], [5, 0], [4, 2], [6, 3], 'backtrack', [2, 1], [0, 0], 'backtrack', 'backtrack', 'backtrack', 'backtrack', [6, 3], [4, 2], [5, 0], 'backtrack', [2, 1], [0, 0], 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', [6, 3], [4, 2], [2, 1], [0, 0], 'backtrack', 'backtrack', [5, 0], [7, 1], [5, 2], 'backtrack', 'backtrack', 'backtrack', 'backtrack', [7, 1], [5, 2], 'backtrack', [5, 0], [4, 2], [2, 1], [0, 0], 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', [4, 2], [2, 1], [0, 0], 'backtrack', 'backtrack', [5, 0], [7, 1], [5, 2], [4, 4], [6, 3], 'backtrack', 'backtrack', 'backtrack', [6, 3], [4, 4], [5, 2], 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', [6, 3], [4, 4], [5, 2], [7, 1], [5, 0], 'backtrack', 'backtrack', 'backtrack', 'backtrack', [7, 1], [5, 0], 'backtrack', [5, 2], [4, 4], 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', 'backtrack', [5, 0], [7, 1], [5, 2], [4, 4], [6, 3], [4, 2], [2, 1], [0, 0], [1, 2], [0, 4], [2, 3]]
+
+
 
 class ChessBoard:
     """ChessBoard class to create and manage the chessboard visualization for the Knight's Tour."""
@@ -31,9 +34,9 @@ class ChessBoard:
         self.rows = rows
         self.columns = columns
         self.root = root
-        self.board_status = [[0 for _ in range(self.columns)] for _ in range(self.rows)] 
         self.board_childrens = {}
         self.board_state = [[{} for _ in range(self.columns)] for _ in range(self.rows)]
+        self.backtrackStatus = []
         self.moveStack = []
         self.buttons = {}
         self.all_knight_moves = all_knight_moves
@@ -42,8 +45,29 @@ class ChessBoard:
         self.image_label = None
         self.paused = False
         self.iter = -1
-        self.done = False
-        
+        self.backtrackIter = 0
+        self.backtrackStart = 0
+    
+    def setupMoves(self):
+        """Set the series of moves to be made by the visualization program
+        """
+        i = 0
+        numBacktrack = 0
+        lastNonBacktrackIndex = 0
+        while i < len(self.all_knight_moves): 
+            knight_move = self.all_knight_moves[i]
+            if knight_move != 'backtrack':
+                numBacktrack = 0
+                self.moveStack.append(knight_move + [i])
+                lastNonBacktrackIndex = i
+                self.backtrackStatus.append(False)
+            else:
+                numBacktrack += 1
+                move = self.moveStack[lastNonBacktrackIndex-numBacktrack]
+                self.moveStack.append([move[0], move[1], move[2]])
+                self.backtrackStatus.append(True)
+            i += 1    
+       
     def populateButtons(self):
         """Create and add control buttons to the Tkinter window."""
         
@@ -115,113 +139,124 @@ class ChessBoard:
                 # Prevent Frame Auto Resizing
                 frame.pack_propagate(False)
                 frame.grid(row=i, column=j, sticky='nsew') 
-                self.board_status[i][j] = frame
+                self.board_state[i][j]['frame'] = frame
 
-    def fillKnight(self, move):       
+    def fillKnight(self, n, m):       
         """Place a knight image on a specific square.
 
         Args:
             move (list): Coordinates [x, y] where the knight should be displayed.
         """
-        self.board_state[move[0]][move[1]]['knight_icon'] = Label(master=self.board_status[move[0]][move[1]], image=self.icon, anchor=CENTER) 
-        self.board_state[move[0]][move[1]]['knight_icon']['bg'] = self.board_state[move[0]][move[1]]['knight_icon'].master['bg']
-        self.board_state[move[0]][move[1]]['knight_icon'].image = self.icon
-        self.board_state[move[0]][move[1]]['knight_icon'].pack(fill="both", expand=1)
+        self.board_state[n][m]['knight_icon'] = Label(master=self.board_state[n][m]['frame'], image=self.icon, anchor=CENTER) 
+        self.board_state[n][m]['knight_icon']['bg'] = self.board_state[n][m]['knight_icon'].master['bg']
+        self.board_state[n][m]['knight_icon'].image = self.icon
+        self.board_state[n][m]['knight_icon'].pack(fill="both", expand=1)
     
-    def fillMoveNumber(self, move):
+    def fillMoveNumber(self, n, m, moveNumber):
         """Display the move number at a specific square.
 
         Args:
             move (list): Coordinates [x, y] where the move number should be displayed.
         """
         # Remove the knight icon and replace it with the move number
-        self.board_state[move[0]][move[1]]['knight_icon'].destroy()
-        self.board_state[move[0]][move[1]]['moveNumber_icon'] = Label(master=self.board_status[move[0]][move[1]], text=f'{self.iter-1}', anchor=CENTER, font=('bold'))
-        self.board_state[move[0]][move[1]]['moveNumber_icon']['bg'] = self.board_state[move[0]][move[1]]['moveNumber_icon'].master['bg']
-        self.board_state[move[0]][move[1]]['moveNumber_icon'].pack(fill="both", expand=1)
+        self.board_state[n][m]['knight_icon'].destroy()
+        self.board_state[n][m]['moveNumber_icon'] = Label(master=self.board_state[n][m]['frame'], text=f'{moveNumber}', anchor=CENTER, font=('bold'))
+        self.board_state[n][m]['moveNumber_icon']['bg'] = self.board_state[n][m]['moveNumber_icon'].master['bg']
+        self.board_state[n][m]['moveNumber_icon'].pack(fill="both", expand=1)
             
-    def nextMove(self):        
+    def nextMove(self):       
         """Move the Knight on the ChessBoard to the next Position."""
-        if self.done:
-            return  # Prevent further moves if the tour is complete
-        
-        if self.iter == len(self.all_knight_moves)-1:
+        if self.iter == len(self.moveStack)-1:
             # If this is the last move, display the end message
             self.iter += 1
-            lastMove = self.all_knight_moves[len(self.all_knight_moves) - 1]
             
             # If the lastmove in the set of moves is backtrack, then no solution is found
-            if lastMove == 'backtrack':
+            if self.backtrackStatus[-1]:
+                self.board_state[self.all_knight_moves[0][0]][self.all_knight_moves[0][1]]['moveNumber_icon'].destroy()
                 self.displayBacktrackingMessage(display=False)
                 self.displayEndMessage(solution=False, display=True)
             
             # Otherwise solution is found
             else:
-                self.fillMoveNumber(lastMove)
+                self.fillMoveNumber(self.moveStack[-1][0], self.moveStack[-1][1], self.moveStack[-1][2])
                 self.displayEndMessage(solution=True, display=True)
             return
-
-        self.iter += 1
         
+        self.iter += 1
+            
         # If there was a previous move, mark that square with the move number
         if self.prevGrid is not None:
-            self.fillMoveNumber(self.prevGrid)
+            self.fillMoveNumber(self.prevGrid[0], self.prevGrid[1], self.prevGrid[2])
         
         # Enable the Previous and Restart buttons
         self.buttons['prev']['state'] = ACTIVE
         self.buttons['restart']['state'] = ACTIVE 
         
         # Get the move for the current iteration
-        move = self.all_knight_moves[self.iter]
+        move = self.moveStack[self.iter]
         
-        # If the move is backtrack, then backtrack until we exit the backtracking sequence
-        if move == 'backtrack':
-            # Handle backtracking by removing the knight from the current square
-            self.displayBacktrackingMessage(True)
+        # If the current move is a backtrack
+        if self.backtrackStatus[self.iter]:
+            # display backtracking message
+            self.displayBacktrackingMessage(display=True)
+            
+            # remove all widgets from the grid we are backtracking to
             self.board_state[self.prevGrid[0]][self.prevGrid[1]]['knight_icon'].destroy()
             self.board_state[self.prevGrid[0]][self.prevGrid[1]]['moveNumber_icon'].destroy()
-            self.moveStack.pop(len(self.moveStack) - 1)
-            move = self.moveStack[-1]
             self.board_state[move[0]][move[1]]['moveNumber_icon'].destroy()
-            self.fillKnight(move)
-        
-        # If a normal move is found, move the knight accordingly
         else:
-            # Move the knight to the new square
-            self.displayBacktrackingMessage(False)
-            self.backtracking = False
-            self.moveStack.append(move)
-            self.fillKnight(move)
+            # if not, remove backtracking message
+            self.displayBacktrackingMessage(False)   
         
-        # Set the previous square as the current move
-        self.prevGrid = move        
+        # fill in the knight at the correct grid and destroy any existing widgets on that square
+        if 'knight_icon' in self.board_state[move[0]][move[1]]:
+            self.board_state[move[0]][move[1]]['knight_icon'].destroy()
+        if 'moveNumber_icon' in self.board_state[move[0]][move[1]]:
+            self.board_state[move[0]][move[1]]['moveNumber_icon'].destroy()
+        self.fillKnight(move[0], move[1])
+        
+        # Set the prevGrid to the current move
+        self.prevGrid = move           
                    
     def prevMove(self):
         """Move to the previous move in the sequence."""
         if self.iter == 1:
             # Disable the Previous button if the knight is at the starting position
             self.buttons['prev']['state'] = DISABLED
-        elif self.iter == len(self.all_knight_moves):
+        elif self.iter == len(self.moveStack):
             # Allow further moves if we were at the last move
             self.displayEndMessage(False)
         
-        # Decrement button press
+        # Get the current move before hitting previous button
+        currentMove = self.moveStack[self.iter]
+        
+        # Destroy knight icon on the current square
+        self.board_state[currentMove[0]][currentMove[1]]['knight_icon'].destroy()
+        
+        # If the current move is a backtracking move, display backtracking message and restore the move number
+        if self.backtrackStatus[self.iter]:
+            self.displayBacktrackingMessage(display=True)
+            self.fillMoveNumber(currentMove[0], currentMove[1], currentMove[2])
+        # Else, remove backtracking message
+        else:
+            self.displayBacktrackingMessage(display=False)
+        
+        # Decrement iter count
         self.iter -= 1
         
-        # Remove the knight and move number from the current square
-        currentMove = self.moveStack.pop()
-        for widget in self.board_state[currentMove[0]][currentMove[1]]:
-            self.board_state[currentMove[0]][currentMove[1]][widget].destroy()
+        # Get the previous move
+        prevMove = self.moveStack[self.iter]
+            
+        # Destroy knight and move number icon from the previous square indicated by the previous move
+        self.board_state[prevMove[0]][prevMove[1]]['knight_icon'].destroy()
+        self.board_state[prevMove[0]][prevMove[1]]['moveNumber_icon'].destroy()
         
-        # Move back to the previous square
-        prevMove = self.moveStack[-1]
-        for widget in self.board_state[prevMove[0]][prevMove[1]]:
-            self.board_state[prevMove[0]][prevMove[1]][widget].destroy()
+        # Add the knight icon to the previous square
+        self.fillKnight(prevMove[0], prevMove[1])
         
-        # Move the knight to the previous square
-        self.fillKnight(prevMove)
+        # Set the previous grid as the previous move
         self.prevGrid = prevMove
-       
+    
     def play(self):  
         """Automatically move the knight across the chessboard according to the moves from the algorithm."""
         self.buttons['play']['state'] = DISABLED
@@ -286,6 +321,7 @@ class ChessBoard:
         self.prevGrid = None
         self.paused = True
         self.done = False
+        self.moveStack = []
         self.runVisualization()
       
     def loadImage(self, filename):
@@ -305,6 +341,7 @@ class ChessBoard:
         """Run the Visualization Program by setting up the grid and buttons."""
         self.populateGrids()
         self.populateButtons()
+        self.setupMoves()
     
 # Test Method
 def main():
@@ -313,8 +350,7 @@ def main():
     root.title("Knight's Tour Visualization")
     root.minsize(width=400, height=400)
     root.configure(background="papaya whip")
-    
-    chessBoard = ChessBoard(3, 3, root, example_no_sol_input)
+    chessBoard = ChessBoard(8, 5, root, example_input_2)
     chessBoard.runVisualization()
     
     root.mainloop()
